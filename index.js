8d843f173962d10e6667531b2c5a8432316114c0
Ashley P <corbinped@gmail.com>
2024-06-30 17:48:20 -0600

    Add track duration

--- index.js
+++ index.js
@@ -11,5 +11,27 @@ const json = await data.json()
 
 for (let i = 0; i < json.toptracks.track.length; i++) {
   const track = json.toptracks.track[i]
-  console.log(`${track.playcount} | ${track.name}`)
+
+  console.log(
+    `${track.playcount} | ${track.name} | ${fancyTimeFormat(track.duration)}`
+  )
+}
+
+function fancyTimeFormat(duration) {
+  // Hours, minutes and seconds
+  const hrs = ~~(duration / 3600)
+  const mins = ~~((duration % 3600) / 60)
+  const secs = ~~duration % 60
+
+  // Output like "1:01" or "4:03:59" or "123:03:59"
+  let ret = ''
+
+  if (hrs > 0) {
+    ret += '' + hrs + ':' + (mins < 10 ? '0' : '')
+  }
+
+  ret += '' + mins + ':' + (secs < 10 ? '0' : '')
+  ret += '' + secs
+
+  return ret
 }
