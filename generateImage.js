45a314c829845474705c55ea8215107361665d9d
Ashley P <corbinped@gmail.com>
2024-06-30 21:18:54 -0600

    Add image width and height params

--- generateImage.js
+++ generateImage.js
@@ -6,25 +6,27 @@ import * as fs from 'fs'
  * @param {string} headerText Text that appears center align at the top of the image.
  * @param {string} leftColumnText Left align text
  * @param {string} rightColumnText Right align text
+ * @param {number} width The width of the image. Default 1000.
+ * @param {number} height The height of the image. Default 1000.
  * @param {string} outputFile The name of the file for the output image.
  */
 export async function generateImage(
     headerText = 'brat',
     leftColumnText,
     rightColumnText,
+    width = 1000,
+    height = 1000,
     outputFile = 'out.png'
 ) {
-    const imageSize = 1000
-
     // make image
-    const img1 = PImage.make(imageSize, imageSize)
+    const img1 = PImage.make(width, height)
 
     // get canvas context
     const ctx = img1.getContext('2d')
 
     // fill with green
     ctx.fillStyle = '#8acf00'
-    ctx.fillRect(0, 0, imageSize, imageSize)
+    ctx.fillRect(0, 0, width, height)
 
     // set font paramaters
     var fnt = PImage.registerFont('C:/Windows/Fonts/Arial.ttf', 'Arial Narrow')
@@ -35,16 +37,16 @@ export async function generateImage(
     ctx.textBaseline = 'middle'
 
     // Title text
-    writeTextMultiline(ctx, headerText, imageSize / 2, 60, 'center')
+    writeTextMultiline(ctx, headerText, width / 2, 60, 'center')
 
     // Left column
     writeTextMultiline(ctx, leftColumnText, 25, 250, 'left')
 
     // Right column
-    writeTextMultiline(ctx, rightColumnText, imageSize - 25, 250, 'right')
+    writeTextMultiline(ctx, rightColumnText, width - 25, 250, 'right')
 
     // Footer text
-    writeTextMultiline(ctx, 'last.fm', imageSize / 2, imageSize - 50, 'center')
+    writeTextMultiline(ctx, 'last.fm', width / 2, height - 50, 'center')
 
     //write to 'out.png'
     PImage.encodePNGToStream(img1, fs.createWriteStream(outputFile))
