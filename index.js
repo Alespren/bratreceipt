72b6278059ee1957b1857deb4be5bb0da1355d37
Ashley P <corbinped@gmail.com>
2024-06-30 22:13:50 -0600

    Tweak image width and max line length

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { getTopTracks } from './getData.js'
 import { generateImage } from './generateImage.js'
 
 const username = process.argv[2] ?? 'alespren'
-const imageWidth = 800
+const imageWidth = 700
 const imageHeight = 1100
 
 const tracks = await getTopTracks(username)
@@ -16,7 +16,7 @@ for (let i = 0; i < tracks.length; i++) {
     leftColumn.push(`${(i + 1).toString().padStart(2, '0')}`)
 
     // if artist + track name are too long, insert new line char
-    const maxLineLength = ~~(imageWidth / 21) // estimate how long a line can be
+    const maxLineLength = ~~(imageWidth / 20) // estimate how long a line can be
     let re = new RegExp(String.raw`.{1,${maxLineLength}}`, 'g')
     const arr = `${track.artist.name} - ${track.name}`.match(re)
 
